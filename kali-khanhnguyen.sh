abc7aa51ef4ac646185013a121bb0af45a460292
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2021-10-11 20:07:30 -0700

    Update kali-khanhnguyen.sh

--- kali-khanhnguyen.sh
+++ kali-khanhnguyen.sh
@@ -3,10 +3,9 @@ termux-setup-storage
 termux-wake-lock
 echo "termux-wake-lock" > ~/.bash_profile
 pkg update -y && pkg upgrade -y
-pkg install wget -y && pkg install proot -y
-pkg install axel -y && pkg install tar
+pkg install wget -y && pkg install proot -y && pkg install tar
 rm -f install-kali-khanhnguyen && rm -f install-nethunter-termux
 wget -O install-kali-khanhnguyen https://tinyurl.com/yj85cahd
 chmod +x install-kali-khanhnguyen
-bash install-kali-khanhnguyen
+bash ~/install-kali-khanhnguyen
 #####
