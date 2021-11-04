7c4f1f5ff84185889e419ad6dce43be1ad839c6f
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2021-11-04 05:32:08 -0700

    Add files via upload

---
+++ kali-khanhnguyenup.sh
@@ -0,0 +1,9 @@
+#kali_arm64_khanhnguyen9872
+cd $HOME
+pkg update -y && pkg upgrade -y
+pkg install wget proot tar pv p7zip -y
+rm -f install-kali-khanhnguyen && rm -f install-nethunter-termux && rm -f update-kali-khanhnguyen
+wget -O update-kali-khanhnguyen https://tinyurl.com/yj85cahd
+chmod +x ~/update-kali-khanhnguyen
+bash ~/update-kali-khanhnguyen
+#####
