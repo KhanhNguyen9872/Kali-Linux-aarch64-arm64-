64324149c9031f3caafc0f00ee860ac434e11ae9
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2021-12-16 08:23:46 +0700

    Update kali-khanhnguyenup.sh

--- kali-khanhnguyenup.sh
+++ kali-khanhnguyenup.sh
@@ -2,8 +2,8 @@
 cd $HOME
 pkg update -y && pkg upgrade -y
 pkg install wget proot tar pv p7zip -y
-rm -f install-kali-khanhnguyen && rm -f install-nethunter-termux && rm -f update-kali-khanhnguyen && rm -f restore-khanhnguyen
-wget -O update-kali-khanhnguyen https://tinyurl.com/368d8bkr
-chmod +x ~/update-kali-khanhnguyen
+rm -f install-kali-khanhnguyen 2> /dev/null && rm -f install-nethunter-termux 2> /dev/null && rm -f update-kali-khanhnguyen 2> /dev/null && rm -f restore-khanhnguyen 2> /dev/null
+wget -O update-kali-khanhnguyen https://tinyurl.com/368d8bkr 2> /dev/null
+chmod +x ~/update-kali-khanhnguyen 2> /dev/null
 bash ~/update-kali-khanhnguyen
 #####
