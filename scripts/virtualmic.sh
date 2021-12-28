d50ea91d41c0eefaf0495a8c6a8cf6a071e7856d
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2021-12-28 10:27:12 +0700

    Update virtualmic.sh

--- scripts/virtualmic.sh
+++ scripts/virtualmic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Script created by KhanhNguyen9872
-virtmic -n VirtualMic rtsp://127.0.0.1:8080 || echo "IP Webcam is not start server!"
+virtmic -n VirtualMic rtsp://127.0.0.1:8080 || echo "Micro app is not start server!"
 echo "Disconnected!"
 exit
