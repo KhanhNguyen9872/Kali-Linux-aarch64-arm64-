35c04a285f646d6dab8ae723cf05912611075a47
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2021-12-30 01:59:52 -0800

    Update virtualmic.sh

--- scripts/virtualmic.sh
+++ scripts/virtualmic.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 # Script created by KhanhNguyen9872
-virtmic -n VirtualMic rtsp://127.0.0.1:8080 || echo "Micro app is not start server!"
-echo "Disconnected!"
+virtmic -n VirtualMic rtsp://127.0.0.1:8080 && echo "Disconnected!" || echo "Micro app is not start server!"
 exit
